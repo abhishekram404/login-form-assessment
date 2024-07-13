8eb473134388b9d41244f9c9bfd6608388199398
Keshav Kishor Ram <ramabishek40@gmail.com>
2024-07-13 21:56:11 +0545

    feat: add basic routing and empty login page

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
+import { routes } from '@utils/routes.tsx'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
+
+const browserRouter = createBrowserRouter(routes)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={browserRouter} />
   </React.StrictMode>,
 )
---
+++ src/utils/routes.tsx
@@ -0,0 +1,9 @@
+import Login from '@pages/Login'
+import { RouteObject } from 'react-router-dom'
+
+export const routes: RouteObject[] = [
+  {
+    path: '/',
+    element: <Login />,
+  },
+]
