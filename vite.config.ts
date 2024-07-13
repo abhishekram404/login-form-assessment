a550f2bffb027c6c52c8570d7181cacc2ce32270
Keshav Kishor Ram <ramabishek40@gmail.com>
2024-07-13 21:43:58 +0545

    config: setup absolute import

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@providers': path.resolve(__dirname, 'src/providers'),
+    },
+  },
 })
