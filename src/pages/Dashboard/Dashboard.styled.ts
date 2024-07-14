8317bef70085524692d7c2ea2615ab77d7211426
Keshav Kishor Ram <ramabishek40@gmail.com>
2024-07-14 21:14:17 +0545

    feat: add helper text on the dasboard

--- src/pages/Dashboard/Dashboard.styled.ts
+++ src/pages/Dashboard/Dashboard.styled.ts
@@ -1,3 +1,20 @@
 import styled from 'styled-components'
 
-export const DashboardStyled = styled.div``
+export const DashboardStyled = styled.div`
+  height: 100%;
+  display: grid;
+  place-items: center;
+
+  > div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 1rem;
+
+    > p {
+      font-size: 1.3rem;
+      font-weight: 500;
+      color: #333;
+    }
+  }
+`
--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,5 +1,16 @@
+import Button from '@components/Button'
+import { Link } from 'react-router-dom'
 import { DashboardStyled } from './Dashboard.styled'
 
 export default function Dashboard() {
-  return <DashboardStyled>Dashboard</DashboardStyled>
+  return (
+    <DashboardStyled>
+      <div>
+        <p>Go to the transactions page to view the transactions.</p>
+        <Link to="/transactions">
+          <Button>Take me to transactions page</Button>
+        </Link>
+      </div>
+    </DashboardStyled>
+  )
 }
