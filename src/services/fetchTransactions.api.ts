18e151facc6a376c383eadcfc616217423f375d6
Keshav Kishor Ram <ramabishek40@gmail.com>
2024-07-15 17:52:30 +0545

    refactor: add the pagination params back to the fetch function

--- src/services/fetchTransactions.api.ts
+++ src/services/fetchTransactions.api.ts
@@ -1,7 +1,11 @@
 import { TransactionsResponseType } from '@pages/Transactions/Transactions.types'
 import axios from 'axios'
 
-export default function fetchTransactions() {
+export default function fetchTransactions(_: number, __: number) {
+  /**
+   * We'd use the page and size arguments to implement the pagination logic
+   * But we don't know the pagination query parameters for the API endpoint
+   */
   return axios.post<TransactionsResponseType>(
     '/report-manager/v1/admin/dashboard/search',
   )
