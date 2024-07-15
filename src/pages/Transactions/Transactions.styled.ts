56d906cb410bc6c006d25f8193945dc0c42ac8e3
Keshav Kishor Ram <ramabishek40@gmail.com>
2024-07-15 17:55:19 +0545

    feat: add page title and subtext on transactions page

--- src/pages/Transactions/Transactions.styled.ts
+++ src/pages/Transactions/Transactions.styled.ts
@@ -2,5 +2,18 @@ import styled from 'styled-components'
 
 export const TransactionsStyled = styled.div`
   height: calc(100dvh - 68px);
-  padding: 1rem;
+  padding: 2rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`
+
+export const PageHeader = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+`
+
+export const PageTitle = styled.h1`
+  font-size: 1.5rem;
 `
--- src/pages/Transactions/Transactions.tsx
+++ src/pages/Transactions/Transactions.tsx
@@ -1,7 +1,11 @@
 import LoadingScreen from '@components/LoadingScreen/LoadingScreen'
 import Table from '@components/Table/Table'
 import useTransactions from '@hooks/useTransactions'
-import { TransactionsStyled } from './Transactions.styled'
+import {
+  PageHeader,
+  PageTitle,
+  TransactionsStyled,
+} from './Transactions.styled'
 import { transactionColumns } from './Transactions.utils'
 
 export default function Transactions() {
@@ -13,6 +17,14 @@ export default function Transactions() {
 
   return (
     <TransactionsStyled>
+      <PageHeader>
+        <PageTitle>Transactions</PageTitle>
+        <p>
+          This page shows all the transactions that have been made. You can use
+          the search bar to find specific transactions.
+        </p>
+      </PageHeader>
+
       <Table
         columns={transactionColumns}
         items={transactions}
